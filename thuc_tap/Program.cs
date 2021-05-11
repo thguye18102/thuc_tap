655e97c8491a83cc1cd4bd5f1efbbe9d6f8ac1a4
Cullen Quinn Edward <huynhnhu28121999@outlook.com>
2021-05-11 10:31:26 +0700

    Test Git commit

--- thuc_tap/Program.cs
+++ thuc_tap/Program.cs
@@ -14,6 +14,8 @@ namespace thuc_tap
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            #region
+            #endregion
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
